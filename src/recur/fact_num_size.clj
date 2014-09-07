81e6a80c703c4037d58f8bff8dad9f42bf7e469d
Oregu <oleg.burykin@gmail.com>
2014-09-07 18:07:54 +0400

    Fix inf loop for gen-dec-dec

--- src/recur/fact_num_size.clj
+++ src/recur/fact_num_size.clj
@@ -40,77 +40,79 @@
                   [(mentionso x h)])])))
 
 (defn eval-expo [exp env selves val size-start size-left]
-  (conde
-   [(symbolo exp)
-    (peano/inco size-left size-start)
-    (lookupo exp env val)]
-   [(fresh [rator rand x body env- a env+ selves+
-            size-start' size-left' size-left'']
-           (== `(~rator ~rand) exp)
-           (peano/inco size-start' size-start)
-           (eval-expo rator env selves `(~'closure ~x ~body ~env-)
-                      size-start' size-left')
-           (eval-expo rand env selves a
-                      size-left' size-left'')
-           (conso `(~x ~a) env- env+)
-           (conso `(~'closure ~x ~body ~env-) selves selves+)
-           (eval-expo body env+ selves+ val
-                      size-left'' size-left))]
-   [(fresh [x body]
-           (== `(~'fn [~x] ~body) exp)
-           (== `(~'closure ~x ~body ~env) val)
-           (peano/inco size-left size-start)
-           (symbolo x)
-           (not-in-envo 'fn env))]
-   [(fresh [selfarg argv prevargv x body env- env+ t
-            size-start' s1 s2]
-           (== `(~'recur ~selfarg) exp)
-           (peano/inco size-start' size-start)
-           (conso `(~'closure ~x ~body ~env-) t selves)
-           (conso `(~x ~argv) env- env+)
-           (not-in-envo 'recur env)
-           (lookupo x env prevargv)
-           (mentionso x selfarg)
-           (eval-expo selfarg env selves argv size-start' size-left)
-           (<o argv prevargv)
-           (eval-expo body env+ selves val s1 s2))]
-   [(fresh [e1 e2 e3 t size-start' size-left' size-left'']
-           (== `(~'if ~e1 ~e2 ~e3) exp)
-           (peano/inco size-start' size-start)
-           (not-in-envo 'if env)
-           (eval-expo e1 env selves t size-start' size-left')
-           (conde
-            [(== true t)
-             ;; TODO we don't go to other branch
-             (peano/inco size-left' size-left'')
-             (eval-expo e2 env selves val size-left'' size-left)]
-            [(== false t)
-             ;; TODO we don't go to other branch
-             (peano/inco size-left' size-left'')
-             (eval-expo e3 env selves val size-left'' size-left)]))]
-   [(fresh [a n size-start']
-           (== `(~'dec ~a) exp)
-           (peano/inco size-start' size-start)
-           (not-in-envo 'dec env)
-           (eval-expo a env selves n size-start' size-left)
-           (+o '(1) val n))]
-   [(fresh [a l size-start' size-left']
-           (== `(~'<=1 ~a) exp)
-           (peano/inco size-start' size-start)
-           (not-in-envo '<=1 env)
-           (conde
-            [(== l '()) (== val true)]
-            [(== l '(1)) (== val true)]
-            [(>1o l) (== val false)])
-           (eval-expo a env selves l size-start' size-left))]
-   [(fresh [a1 a2 va1 va2 v size-start' size-left']
-           (== `(~'* ~a1 ~a2) exp)
-           (== v val)
-           (peano/inco size-start' size-start)
-           (not-in-envo '* env)
-           (*o va1 va2 v)
-           (eval-expo a1 env selves va1 size-start' size-left')
-           (eval-expo a2 env selves va2 size-left' size-left))]))
+  (all
+   #_(trace-lvars "eval" [exp val size-start size-left])
+   (conde
+     [(symbolo exp)
+      (peano/inco size-left size-start)
+      (lookupo exp env val)]
+     [(fresh [rator rand x body env- a env+ selves+
+              size-start' size-left' size-left'']
+             (== `(~rator ~rand) exp)
+             (peano/inco size-start' size-start)
+             (eval-expo rator env selves `(~'closure ~x ~body ~env-)
+                        size-start' size-left')
+             (eval-expo rand env selves a
+                        size-left' size-left'')
+             (conso `(~x ~a) env- env+)
+             (conso `(~'closure ~x ~body ~env-) selves selves+)
+             (eval-expo body env+ selves+ val
+                        size-left'' size-left))]
+     [(fresh [x body]
+             (== `(~'fn [~x] ~body) exp)
+             (== `(~'closure ~x ~body ~env) val)
+             (peano/inco size-left size-start)
+             (symbolo x)
+             (not-in-envo 'fn env))]
+     [(fresh [selfarg argv prevargv x body env- env+ t
+              size-start' s1 s2]
+             (== `(~'recur ~selfarg) exp)
+             (peano/inco size-start' size-start)
+             (conso `(~'closure ~x ~body ~env-) t selves)
+             (conso `(~x ~argv) env- env+)
+             (not-in-envo 'recur env)
+             (lookupo x env prevargv)
+             (mentionso x selfarg)
+             (eval-expo selfarg env selves argv size-start' size-left)
+             (<o argv prevargv)
+             (eval-expo body env+ selves val s1 s2))]
+     [(fresh [e1 e2 e3 t size-start' size-left' size-left'']
+             (== `(~'if ~e1 ~e2 ~e3) exp)
+             (peano/inco size-start' size-start)
+             (not-in-envo 'if env)
+             (eval-expo e1 env selves t size-start' size-left')
+             (conde
+              [(== true t)
+               ;; TODO we don't go to other branch
+               (peano/inco size-left' size-left'')
+               (eval-expo e2 env selves val size-left'' size-left)]
+              [(== false t)
+               ;; TODO we don't go to other branch
+               (peano/inco size-left' size-left'')
+               (eval-expo e3 env selves val size-left'' size-left)]))]
+     [(fresh [a n size-start']
+             (== `(~'dec ~a) exp)
+             (peano/inco size-start' size-start)
+             (not-in-envo 'dec env)
+             (eval-expo a env selves n size-start' size-left)
+             (+o '(1) val n))]
+     [(fresh [a l size-start' size-left']
+             (== `(~'<=1 ~a) exp)
+             (peano/inco size-start' size-start)
+             (not-in-envo '<=1 env)
+             (conde
+              [(== l '()) (== val true)]
+              [(== l '(1)) (== val true)]
+              [(>1o l) (== val false)])
+             (eval-expo a env selves l size-start' size-left))]
+     [(fresh [a1 a2 va1 va2 v size-start' size-left']
+             (== `(~'* ~a1 ~a2) exp)
+             (== v val)
+             (peano/inco size-start' size-start)
+             (not-in-envo '* env)
+             (eval-expo a1 env selves va1 size-start' size-left')
+             (eval-expo a2 env selves va2 size-left' size-left)
+             (*o va1 va2 v))])))
 
 (defn evalo [exp val size-start]
   (all #_(noo 'closure exp)
